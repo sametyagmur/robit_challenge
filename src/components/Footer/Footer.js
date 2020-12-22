21ca0fae953be788ddbed4d71059daed19494127
sametyagmur <yagmursamet@gmail.com>
2020-12-22 23:18:47 +0300

    final commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,11 +7,11 @@ class Footer extends Component{
 <div className="footer-area">
     <div className="container-fluid">
     <div className="register row">
-        <div className="col-lg-9">
+        <div className="col-lg-9 col-md-9">
             <h4>Robit Register, free account</h4>
             <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical.</p>
         </div>
-        <div className="footer-register-btn col-lg-3">
+        <div className="footer-register-btn col-lg-3 col-md-3">
             <button className="btn btn-primary" type="button">Free Register</button>
         </div>
     </div>
@@ -29,7 +29,7 @@ class Footer extends Component{
         <div className="footer-menu-right col-lg-5">
             <a href="/menu01">Menu 04</a>
             <a href="/menu01">Menu 05</a>
-            <a href="/menu01">Menu 05</a> 
+            <a href="/menu01">Menu 06</a> 
         </div>
         </div>
         <div className="copyright row">
